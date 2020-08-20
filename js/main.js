58d05588f6c3be59ebc644ae552b92f0a4cd97a8
unknown <timurh434@gmail.com>
2020-08-20 19:37:26 +0300

    fix scroll

--- js/main.js
+++ js/main.js
@@ -1,17 +1,19 @@
-    var btnScrollDown = document.querySelector('.scroll_down');
+// I took it from stackoverflow
+
+var btnScrollDown = document.querySelector('.scroll_down');
 
 function scrollDown() {
-        var windowCoords = document.documentElement.clientHeight;
-       
-        (function scroll() {
-            if (Math.ceil(window.pageYOffset) < Math.ceil(windowCoords)) {
-                window.scrollBy(0, 10);
-                setTimeout(scroll, 2);
-            }
-             if (Math.ceil(window.pageYOffset) > Math.ceil(windowCoords)) {
-                 window.scrollTo(0, Math.ceil(windowCoords));
-            }
-        })();
-    };
+    var windowCoords = document.documentElement.clientHeight;
+
+    (function scroll() {
+        if (Math.ceil(window.pageYOffset) < Math.ceil(windowCoords)) {
+            window.scrollBy(0, 10);
+            setTimeout(scroll, 2);
+        }
+            if (Math.ceil(window.pageYOffset) > Math.ceil(windowCoords)) {
+                window.scrollTo(0, Math.ceil(windowCoords));
+        }
+    })();
+};
 
      btnScrollDown.addEventListener('click', scrollDown);
